164861ea354f1a4fbd21afb62d8b0f35dd0fb4a7
alvapereyra <alvaropereyragamez@gmail.com>
2023-05-01 18:26:09 -0300

    .

---
+++ src/router/views.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import ProductManager from "../controller/ProductManager.js"
+
+const product = new ProductManager()
+const viewRouter = Router();
+
+viewRouter.get('/realtimeproducts', async (req, res) => {
+    res.render('realTimeProducts', { products: await product.getProducts() });
+});
